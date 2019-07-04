acaf8c7b8156d2b89c34314692aa6f73b50f2f0e
Bharghava Varun Ayada <abvarun226@gmail.com>
2019-07-04 12:05:15 +0530

    circular array loop

--- leetcode/Problem457.py
+++ leetcode/Problem457.py
@@ -2,25 +2,41 @@ from typing import List
 
 def circularArrayLoop(nums: List[int]) -> bool:
     n = len(nums)
+    
+    if nums is None or n < 2: return False
+
     for i in range(0, n):
-        s = i
-        f = i
+        if nums[i] == 0: return False
 
-        while nums[s] * nums[next(nums, f)] > 0 and nums[s] * nums[next(nums, next(nums, f))] > 0:
-            s = next(nums, s)
-            f = next(nums, next(nums, f))
+        # s = slow pointer and f = fast pointer
+        s, f = i, follow(nums, i)
+
+        # Check if loop is moving in single direction
+        while nums[f] * nums[i] > 0 and nums[i] * nums[follow(nums, f)] > 0:
+            # If slow is equal to fast pointers
             if s == f:
-                return not s == next(nums, s)
+                # and there is more than one element in the loop
+                if s == follow(nums, s): break
+                return True
+            
+            # Increment slow and fast pointers
+            s = follow(nums, s)
+            f = follow(nums, follow(nums, f))
 
-        j, val = i, nums[i]
-        while nums[j] * val > 0:
-            j, nums[j] = next(nums, j), 0
+        s = i
+        while nums[s] * nums[i] > 0:
+            nums[s], s = 0, follow(nums, s)
 
     return False
 
-def next(nums: List[int], i: int) -> int:
+def follow(nums: List[int], i: int) -> int:
     return (len(nums) + nums[i] + i) % len(nums)
 
+inp = [-1]
+assert not circularArrayLoop(inp)
+
+inp = [1, 1]
+assert circularArrayLoop(inp)
 
 inp = [2,-1,1,2,2]
 assert circularArrayLoop(inp)
