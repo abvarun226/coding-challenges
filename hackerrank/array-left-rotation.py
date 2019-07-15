088e45f008ee545405c928a9e2a1581e66347ac8
Bharghava Varun Ayada <abvarun226@gmail.com>
2019-07-15 09:39:24 +0530

    array left rotation

--- hackerrank/array-left-rotation.py
+++ hackerrank/array-left-rotation.py
@@ -20,7 +20,8 @@ def gcd(a, b):
 
 def rotation(a, d):
     ''' Optimal with extra space: time O(n) and space O(d) '''
-    return a[d:len(a)] + a[:d]
+    d = d % len(a)
+    return a[d:] + a[:d]
 
 inp = [
     ("1 2 3 4 5 6 7 8 9 10 11 12", 3, "4 5 6 7 8 9 10 11 12 1 2 3"),
