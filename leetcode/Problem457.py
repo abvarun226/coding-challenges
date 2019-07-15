b813caf188f1e72139eb792a6ea1767f4f26ee63
Bharghava Varun Ayada <abvarun226@gmail.com>
2019-07-15 11:18:05 +0530

    circular array loop

--- leetcode/Problem457.py
+++ leetcode/Problem457.py
@@ -9,27 +9,28 @@ def circularArrayLoop(nums: List[int]) -> bool:
         if nums[i] == 0: return False
 
         # s = slow pointer and f = fast pointer
-        s, f = i, follow(nums, i)
+        s, f = i, nextElem(nums, i)
 
         # Check if loop is moving in single direction
-        while nums[f] * nums[i] > 0 and nums[i] * nums[follow(nums, f)] > 0:
+        while nums[f] * nums[i] > 0 and nums[i] * nums[nextElem(nums, f)] > 0:
             # If slow is equal to fast pointers
             if s == f:
                 # and there is more than one element in the loop
-                if s == follow(nums, s): break
+                if s == nextElem(nums, s): break
                 return True
             
             # Increment slow and fast pointers
-            s = follow(nums, s)
-            f = follow(nums, follow(nums, f))
+            s = nextElem(nums, s)
+            f = nextElem(nums, nextElem(nums, f))
 
+        # mark every index on the path that failed with 0
         s = i
         while nums[s] * nums[i] > 0:
-            nums[s], s = 0, follow(nums, s)
+            nums[s], s = 0, nextElem(nums, s)
 
     return False
 
-def follow(nums: List[int], i: int) -> int:
+def nextElem(nums: List[int], i: int) -> int:
     return (len(nums) + nums[i] + i) % len(nums)
 
 inp = [-1]
